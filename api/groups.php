f845e6fb3b79066a659e5a32d7c29a01b82edc47
Anton Kiyatkin <a.kiyatkin@itlf.ru>
2019-05-11 01:07:23 +0400

    fix

--- api/groups.php
+++ api/groups.php
@@ -1,5 +1,41 @@
 <?php
-use infrajs\load\Load;
+
+use akiyatkin\showcase\Showcase;
+use infrajs\ans\Ans;
+use infrajs\excel\Xlsx;
+
+$ans = array();
+$md = Showcase::initMark($ans);
+$ans['root'] = Showcase::getGroup();
+$isempty = Ans::GET('empty','bool', false);
+
+if (!$isempty) {
+	Xlsx::runGroups($ans['root'], function &(&$group, $i, &$parent) {
+		if ($parent && empty($group['childs']) && !$group['count']) {
+			unset($parent['childs'][$i]);
+		}
+		$r = null;
+		return $r;
+	}, true);
+}
+
+$group = false;
+foreach ($md['group'] as $group => $one) break;
+if ($group) {
+	$group = Showcase::getGroup($group);
+	$path = $group['path'];
+
+	Xlsx::runGroups($ans['root'], function &(&$group) use ($path) {
+		$r = null;
+		if (in_array($group['group_nick'], $path)) {
+			$group['active'] = true;
+		}
+		return $r;
+	}, true);
+	$ans['path'] = $group['path'];
+}
+return Ans::ret($ans);
+/*use infrajs\load\Load;
 use infrajs\rest\Rest;
 use infrajs\ans\Ans;
 use akiyatkin\showcase\Catalog;
@@ -24,4 +60,4 @@ foreach ($root['childs'] as $k=>$group) {
 	unset($root['childs'][$k]['parent_nick']);
 }
 $ans['list'] = $root['childs'];
-return Ans::ret($ans);
+return Ans::ret($ans);*/
