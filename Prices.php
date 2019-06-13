a4cc52a6ff0f783068ae769a4c839f1ae9ef21ef
Anton Kiyatkin <a.kiyatkin@itlf.ru>
2019-06-13 13:17:57 +0400

    fix

--- Prices.php
+++ Prices.php
@@ -446,7 +446,7 @@ class Prices {
 		$duration = (time() - $time);
 		
 		foreach($ans as $i=>$val){
-			if(sizeof($ans[$i]) > 500) $ans[$i] = sizeof($ans[$i]);
+			if (is_array($ans[$i]) && sizeof($ans[$i]) > 500) $ans[$i] = sizeof($ans[$i]);
 		}
 
 		$jsonans = Load::json_encode($ans);
